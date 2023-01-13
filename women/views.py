ec1fabdc13328e9aadc1f013afdd943965a256c9
chudilo4 <chipalinchik@gmail.com>
2023-01-13 20:12:31 +0500

    Базовый класс APIView для представлений

--- women/views.py
+++ women/views.py
@@ -1,5 +1,8 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from women.models import Women
 from women.serializers import WomenSerializer
@@ -8,6 +11,20 @@ from women.serializers import WomenSerializer
 # Create your views here.
 
 
-class WomenAPIView(ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+# class WomenAPIView(ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
