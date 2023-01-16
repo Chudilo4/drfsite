d868c8552a79e720f5c260c70fb5584c3b009f7b
chudilo4 <chipalinchik@gmail.com>
2023-01-16 13:37:42 +0500

    Роутеры: SimpleRouter и DefaultRouter

--- women/views.py
+++ women/views.py
@@ -1,11 +1,12 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
+from rest_framework.decorators import action
 from rest_framework.generics import ListAPIView, ListCreateAPIView, UpdateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-from women.models import Women
+from women.models import Women, Category
 from women.serializers import WomenSerializer
 
 
@@ -13,8 +14,19 @@ from women.serializers import WomenSerializer
 
 
 class WomenViewSet(ReadOnlyModelViewSet):
-    queryset = Women.objects.all()
+    # queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        if not pk:
+            return Women.objects.all()[:3]
+        return Women.objects.filter(pk=pk)
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
 
 # class WomenAPIList(ListCreateAPIView):
 #     queryset = Women.objects.all()
